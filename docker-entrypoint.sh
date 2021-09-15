057d17a29601fd4ba5d2df7833d438a150b42994
mihalin <stopa3.10.1996@mail.ru>
2021-09-15 23:20:08 +0300

    minor changes, auto-certificate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 if [[ -z "${CUSTOM_CERT}" ]]; then
+  echo "Use custom certificate"
   if [ ! -f /cert/private.key ]; then
+    echo "Generate new certificate"
     openssl req -newkey rsa:2048 -sha256 -nodes -keyout /cert/private.key -x509 -days 1000 -out /cert/public.pem -subj "/C=US/ST=Berlin/L=Berlin/O=my_org/CN=my_cn"
   fi
 fi
