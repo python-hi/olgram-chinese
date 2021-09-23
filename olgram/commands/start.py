0d8081be35831cbde6856e953f6183e0d9732606
mihalin <stopa3.10.1996@mail.ru>
2021-09-24 02:57:57 +0300

    docks in start message

--- olgram/commands/start.py
+++ olgram/commands/start.py
@@ -20,7 +20,8 @@ async def start(message: types.Message, state: FSMContext):
     # TODO: locale
 
     await message.answer(dedent("""
-    Olgram Bot — это конструктор ботов обратной связи в Telegram.
+    Olgram Bot — это конструктор ботов обратной связи в Telegram. Подробнее 
+    <a href="https://olgram.readthedocs.io">читайте здесь</a> 
 
     Используйте эти команды, чтобы управлять этим ботом:
 
@@ -28,7 +29,7 @@ async def start(message: types.Message, state: FSMContext):
     /mybots - управление ботами
 
     /help - помощь
-    """))
+    """), parse_mode="html")
 
 
 @dp.message_handler(commands=["help"], state="*")
