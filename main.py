654d0047dafb1d26da9e8e3fd05093ad630fcd23
mihalin <stopa3.10.1996@mail.ru>
2022-03-29 23:17:25 +0300

    promo first iteration

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import olgram.commands.start  # noqa: F401
 import olgram.commands.menu  # noqa: F401
 import olgram.commands.bot_actions  # noqa: F401
 import olgram.commands.info  # noqa: F401
+import olgram.commands.promo  # noqa: F401
 from locales.locale import _
 
 from server.server import main as server_main
