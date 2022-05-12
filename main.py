15083fed8d4d1cd7e64ecb861963b26263a8dd4c
mihalin <stopa3.10.1996@mail.ru>
2022-05-12 15:59:37 +0300

    notification, first iteration

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import olgram.commands.menu  # noqa: F401
 import olgram.commands.bot_actions  # noqa: F401
 import olgram.commands.info  # noqa: F401
 import olgram.commands.promo  # noqa: F401
+import olgram.commands.admin  # noqa: F401
 from locales.locale import _
 
 from server.server import main as server_main
